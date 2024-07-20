7bcffd7a8e2d57565737f86be7b43b4d2a407062
Aleksandar Milic <amilic018989@gmail.com>
2024-07-20 02:04:08 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,6 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use Shogy\LaravelLogDownloader\Http\Controllers\LogDownloadController;
+
+Route::middleware(['web', 'auth'])->get('/download-logs', [LogDownloadController::class, 'downloadLogs']);
